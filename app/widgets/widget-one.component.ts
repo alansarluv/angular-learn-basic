105db2d7f8a6f909a48c0e30296ce05736553576
alansarluv <alan.sarluf@gmail.com>
2017-03-28 19:06:26 +0700

    another way to share service via presentation component

--- app/widgets/widget-one.component.ts
+++ app/widgets/widget-one.component.ts
@@ -1,12 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'widget-one',
     template: `
         <div *ngIf="selected">This is widget number one</div>
+        <div>{{messageService}}</div>
+        <br> <hr>
     `
 })
 
 export class WidgetOne {
     selected = true;
+    @Input() messageService;
 }
